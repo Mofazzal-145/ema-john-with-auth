cb40bcf4965c347c1196f7cc1248314319d4fba0
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-24 16:31:22 +0600

    added many file

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,11 @@
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
      const {name, img, ratings, price, seller} = props.product;
+     console.log(props)
     return (
         <div className='product'>
             <img src={img} alt='' ></img>
@@ -12,8 +15,9 @@ const Product = (props) => {
                <p><small>Seller:{seller}</small></p>
                <p><small>Rating:{ratings} stars</small></p>
              </div>
-             <button className='btn-cart'>
+             <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
                  <p>Add To Cart</p>
+                 <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
              </button>
         </div>
     );
