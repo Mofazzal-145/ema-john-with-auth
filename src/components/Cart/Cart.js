b565a0fd601f0c6b86b96450c942e76d103b5b74
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-25 01:26:21 +0600

    added total and shipping price in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2)
     return (
-        <div>
+        <div className='cart'>
               <h3>Order Summary</h3>
-                <p>Selected Items: {cart.length}</p>
+              <p>Selected Items: {cart.length}</p>
+              <p>Total Price:${total}</p>
+              <p>Shipping Cost:$ {shipping} </p>
+              <p>Tax:$ {tax}</p>
+              <h4>Grand Total: $</h4>
         </div>
     );
 };
